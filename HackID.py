4423359f8e003a4155128c1b9d4f6d9e25f55159
⎝◣⏝◢⎠SANTRI-CYBER-SANTUY⎝◣⏝◢⎠ <79558956+AKKUSANTUY@users.noreply.github.com>
2021-04-04 09:22:35 +0700

    Update HackID.py

--- HackID.py
+++ HackID.py
@@ -1,3 +1,69 @@
+import os, sys
+
+print ("\033[30;1mSilahkan Masukkan Username & Password Scriptnya")
+
+print ("\033[33;1matau silahkan Hubungi Author ")
+
+username = 'SANTRI'      
+
+password = 'CYBER'
+
+
+
+def restart():
+
+	ngulang = sys.executable
+
+	os.execl(ngulang, ngulang, *sys.argv)
+
+
+
+def main():
+
+	uname = raw_input("username : ")
+
+	if uname == username:
+
+		pwd = raw_input("password : ")
+
+
+
+		if pwd == password:
+
+			print "\033[32;1mAkhirnya password bener juga tuh", 
+
+			sys.exit()
+
+
+
+		else:
+
+			print "\033[31;1mPasswordnya salah woiii... [?]\033[33;1m"
+
+			print "Silahkan segera log-in kembali...!!\n"
+
+			restart()
+
+
+
+	else:
+
+		print "\033[30;1mUsernamenya salah woiii... [?]\033[31;1m"
+
+		print "Silahkan segera log-in kembali...!!\n"
+
+		restart()
+
+
+
+try:
+
+	main()
+
+except KeyboardInterrupt:
+
+	os.system('clear')
+
 #_*_coding:UTF-8_*_
 
 from os import system as sis
